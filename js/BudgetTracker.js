0d60be6caffed93ea316fb389b369912b7c65347
Shivani <shivanijaiswal528@gmail.com>
2023-09-17 20:50:48 -0700

    j

---
+++ js/BudgetTracker.js
@@ -0,0 +1,33 @@
+export default class BudgetTracker {
+    constructor(querySelectorString) {
+        this.root = document.querySelector(querySelectorString);
+        console.log(this.root);
+    }
+    static html() {
+
+    }
+    static entryHtml() {
+
+    }
+    load() {
+
+    }
+    updateSummary() {
+
+    }
+    save() {
+
+    }
+    addEntry(entry = {}) {
+         
+    }
+    getEntryRows() {
+
+    }
+    onNewEntryBtnClick() {
+
+    }
+    onDeleteEntryBtnClick(e) {
+
+    }
+}
