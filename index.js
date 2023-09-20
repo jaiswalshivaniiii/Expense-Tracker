69cb259d9431f48781d6cfe9da0f5a6a12051dab
Shivani <shivanijaiswal528@gmail.com>
2023-09-19 17:28:33 -0700

    Corrected

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 let totalAmount = document.getElementById("total-amount");
 let userAmount = document.getElementById("user-amount");
 const checkAmountButton = document.getElementById("check-amount");
-const totalAmountButton = document.getElementById("total-check-amount");
+const totalAmountButton = document.getElementById("total-amount-button");
 const productTitle = document.getElementById("product-title");
 const errorMessage = document.getElementById("budget-error");
 const productTitleError = document.getElementById("product-title-error");
-const productCostError = document.getElementById("product-cost-error");
+// const productCostError = document.getElementById("product-cost-error");
 const amount = document.getElementById("amount");
 const expenditureValue = document.getElementById("expenditure-value");
 const balanceValue = document.getElementById("balance-amount");
@@ -14,7 +14,7 @@ let tempAmount = 0;
 
 //set budget part
 totalAmountButton.addEventListener("click", () => {
-    tempAmount = totalAmount.ariaValueMax;
+    tempAmount = totalAmount.value;
     //empty or negative input
     if (tempAmount === "" || tempAmount < 0) {
         errorMessage.classList.remove("hide");
@@ -27,4 +27,76 @@ totalAmountButton.addEventListener("click", () => {
         //clear Input Box
         totalAmount.value = "";
     }
-})
+});
+
+//Function To Disable Edit and Delete Button
+const disableButtons = (bool) => {
+    let editButtons = document.getElementsByClassName("edit");
+    Array.from(editButtons).forEach((element) => {
+        element.disabled = bool;
+    });
+};
+
+//Function To Modify List Elements
+const modifyElement = (element, edit = false) => {
+    let parentDiv = element.parentElement;
+    let currentBalance = balanceValue.innerText;
+    let currentExpense = expenditureValue.innerText;
+    let parentAmount = parentDiv.querySelector(".product:nth-child(2)").innerText;
+    if(edit){
+        let parentText = parentDiv.querySelector(".product").innerText;
+        productTitle.value = parentText;
+        userAmount.value = parentAmount;
+        disableButtons(true);
+    }
+    balanceValue.innerText = parseInt(currentBalance) + parseInt(parentAmount);
+    expenditureValue.innerText = parseInt(currentExpense) - parseInt(parentAmount);
+    parentDiv.remove();
+};
+
+//Function to create List
+const listCreator = (expenseName, expenseValue) => {
+    let sublistContent = document.createElement("div");
+    sublistContent.classList.add("sublist-content","flex-space");
+    list.appendChild(sublistContent);
+    sublistContent.innerHTML = `<p class="product">${expenseName}</p><p class="product">${expenseValue}</p>`;
+    let editButton = document.createElement("button");
+    editButton.classList.add("fa-solid", "fa-pen-to-square", "edit");
+    editButton.style.fontSize = "24px";
+    editButton.addEventListener("click", () => {
+        modifyElement(editButton, true);
+    });
+    let deleteButton = document.createElement("button");
+    deleteButton.classList.add("fa-solid","fa-trash-can","delete");
+    deleteButton.style.fontSize = "24px";
+    deleteButton.addEventListener("click", () => {
+        modifyElement(deleteButton);
+    });
+    sublistContent.appendChild(editButton);
+    sublistContent.appendChild(deleteButton);
+    document.getElementById("list").appendChild(sublistContent);
+};
+
+//Function To Add Expenses
+checkAmountButton.addEventListener("click", () => {
+    //empty checks
+    if(!userAmount.value || !productTitle.value) {
+        productTitleError.classList.remove("hide");
+        return false;
+    }
+    //Enable buttons
+    disableButtons(false);
+    //Expense
+    let expenditure = parseInt(userAmount.value);
+    //Total expense (existing + new)
+    let sum = parseInt(expenditureValue.innerText) + expenditure;
+    expenditureValue.innerText = sum;
+    //Total balance(budget - total expense)
+    const totalBalance = tempAmount - sum;
+    balanceValue.innerText = totalBalance;
+    //Create list
+    listCreator(productTitle.value, userAmount.value);
+    //Empty inputs
+    productTitle.value = "";
+    userAmount.value = "";
+});
